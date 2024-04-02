765058246867fe59c0cdcf10f7a8cc0017e48b85
ZhannaVi <zhanna08012005@mail.ru>
2024-04-02 16:15:34 +0300

    Добавьте файлы проекта.

---
+++ Popova_0.5/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Popova_0._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.grpLevel = new System.Windows.Forms.GroupBox();
+            this.AdvancedLevel = new System.Windows.Forms.RadioButton();
+            this.IntermediateLevel = new System.Windows.Forms.RadioButton();
+            this.beginnerLevel = new System.Windows.Forms.RadioButton();
+            this.btnGame = new System.Windows.Forms.Button();
+            this.grpLevel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(32, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(630, 630);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.Color.LightCyan;
+            this.btnCheck.Location = new System.Drawing.Point(690, 60);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(223, 82);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check input";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.PowderBlue;
+            this.btnClear.Location = new System.Drawing.Point(734, 148);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(142, 49);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear Input";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // grpLevel
+            // 
+            this.grpLevel.BackColor = System.Drawing.Color.LightBlue;
+            this.grpLevel.Controls.Add(this.AdvancedLevel);
+            this.grpLevel.Controls.Add(this.IntermediateLevel);
+            this.grpLevel.Controls.Add(this.beginnerLevel);
+            this.grpLevel.Location = new System.Drawing.Point(734, 203);
+            this.grpLevel.Name = "grpLevel";
+            this.grpLevel.Size = new System.Drawing.Size(142, 188);
+            this.grpLevel.TabIndex = 2;
+            this.grpLevel.TabStop = false;
+            this.grpLevel.Text = "Level";
+            // 
+            // AdvancedLevel
+            // 
+            this.AdvancedLevel.AutoSize = true;
+            this.AdvancedLevel.Location = new System.Drawing.Point(7, 145);
+            this.AdvancedLevel.Name = "AdvancedLevel";
+            this.AdvancedLevel.Size = new System.Drawing.Size(105, 24);
+            this.AdvancedLevel.TabIndex = 0;
+            this.AdvancedLevel.TabStop = true;
+            this.AdvancedLevel.Text = "Advanced";
+            this.AdvancedLevel.UseVisualStyleBackColor = true;
+            // 
+            // IntermediateLevel
+            // 
+            this.IntermediateLevel.AutoSize = true;
+            this.IntermediateLevel.Location = new System.Drawing.Point(7, 103);
+            this.IntermediateLevel.Name = "IntermediateLevel";
+            this.IntermediateLevel.Size = new System.Drawing.Size(124, 24);
+            this.IntermediateLevel.TabIndex = 0;
+            this.IntermediateLevel.TabStop = true;
+            this.IntermediateLevel.Text = "Intermediate";
+            this.IntermediateLevel.UseVisualStyleBackColor = true;
+            // 
+            // beginnerLevel
+            // 
+            this.beginnerLevel.AutoSize = true;
+            this.beginnerLevel.Location = new System.Drawing.Point(7, 61);
+            this.beginnerLevel.Name = "beginnerLevel";
+            this.beginnerLevel.Size = new System.Drawing.Size(96, 24);
+            this.beginnerLevel.TabIndex = 0;
+            this.beginnerLevel.TabStop = true;
+            this.beginnerLevel.Text = "beginner";
+            this.beginnerLevel.UseVisualStyleBackColor = true;
+            // 
+            // btnGame
+            // 
+            this.btnGame.BackColor = System.Drawing.Color.PowderBlue;
+            this.btnGame.Location = new System.Drawing.Point(734, 419);
+            this.btnGame.Name = "btnGame";
+            this.btnGame.Size = new System.Drawing.Size(142, 49);
+            this.btnGame.TabIndex = 1;
+            this.btnGame.Text = "New game";
+            this.btnGame.UseVisualStyleBackColor = false;
+            this.btnGame.Click += new System.EventHandler(this.btnGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 795);
+            this.Controls.Add(this.grpLevel);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnGame);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "sudoku";
+            this.grpLevel.ResumeLayout(false);
+            this.grpLevel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.GroupBox grpLevel;
+        private System.Windows.Forms.RadioButton AdvancedLevel;
+        private System.Windows.Forms.RadioButton IntermediateLevel;
+        private System.Windows.Forms.RadioButton beginnerLevel;
+        private System.Windows.Forms.Button btnGame;
+    }
+}
+
---
+++ Popova_0.5/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Popova_0._5
+{
+
+    public partial class Form1 : Form
+    {
+        int WrongCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            createCells();
+            startNewGame();
+        }
+        private void startNewGame()
+        {
+            loadValues();
+            showRandomValuesHints(45);
+        }
+
+        private void loadValues()
+        {
+            // Очищает значения в каждой ячейке
+            foreach (var cell in cells)
+            {
+                cell.Value = 0;
+                cell.Clear();
+            }
+
+            // Этот метод будет вызываться рекурсивно 
+            // до тех пор, пока не будут найдены подходящие значения для каждой ячейки
+            findValueForNextCell(0, -1);
+        }
+
+        Random random = new Random();
+
+        private bool findValueForNextCell(int i, int j)
+        {
+            // Увеличьте значения i и j, чтобы перейти к следующей ячейке
+            // и, если столбец заканчивается, перейдет к следующей строке
+            if (++j > 8)
+            {
+                j = 0;
+
+                // Exit 
+                if (++i > 8)
+
+                {
+                    return true;
+                }
+
+
+
+            }
+
+            var value = 0;
+            var numsLeft = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Find a random and valid number for the cell and go to the next cell 
+            // and check if it can be allocated with another random and valid number
+            do
+            {
+                // Находит случайные и действительные номера для ячейки и переходит к следующей ячейке 
+                // и проверяет, можно ли присвоить ей другой случайный и действительный номер
+                if (numsLeft.Count < 1)
+                {
+                    cells[i, j].Value = 0;
+                    return false;
+                }
+
+                // Выберает случайное число из числа, оставшегося в списке
+                value = numsLeft[random.Next(0, numsLeft.Count)];
+                cells[i, j].Value = value;
+
+                // Удаляет выделенное значение из списка
+                numsLeft.Remove(value);
+            }
+            while (!isValidNumber(value, i, j) || !findValueForNextCell(i, j));
+
+           
+            cells[i, j].Text = value.ToString();
+
+            return true;
+        }
+
+        private bool isValidNumber(int value, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                // Проверяет все ячейки в вертикальном направлении
+                if (i != y && cells[x, i].Value == value)
+                    return false;
+
+                // Проверит все ячейки в горизонтальном направлении
+                if (i != x && cells[i, y].Value == value)
+                    return false;
+            }
+
+            // Проверяет все ячейки в определенном блоке
+            for (int i = x - (x % 3); i < x - (x % 3) + 3; i++)
+            {
+                for (int j = y - (y % 3); j < y - (y % 3) + 3; j++)
+                {
+                    if (i != x && j != y && cells[i, j].Value == value)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+
+
+        Sudoku[,] cells = new Sudoku[9, 9];
+
+
+        private void showRandomValuesHints(int hintsCount)
+        {
+            // Отображает значение в случайных ячейках
+            // Количество подсказок зависит от выбранного игроком уровня
+            for (int i = 0; i < hintsCount; i++)
+            {
+                var rX = random.Next(9);
+                var rY = random.Next(9);
+
+                // Оформдяем ячейки с подсказками по-другому и
+                // заблокируем ячейку, чтобы игрок не мог редактировать значение
+                cells[rX, rY].Text = cells[rX, rY].Value.ToString();
+                cells[rX, rY].ForeColor = Color.Black;
+                cells[rX, rY].IsLocked = true;
+            }
+        }
+        private void createCells()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    // Создает 81 ячейку со стилями и местоположениями на основе индекса
+                    cells[i, j] = new Sudoku();
+                    cells[i, j].Font = new Font(SystemFonts.DefaultFont.FontFamily, 20);
+                    cells[i, j].Size = new Size(40, 40);
+                    cells[i, j].ForeColor = SystemColors.ControlDarkDark;
+                    cells[i, j].Location = new Point(i * 40, j * 40);
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? SystemColors.Control : Color.LightGray;
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].FlatAppearance.BorderColor = Color.Black;
+                    cells[i, j].X = i;
+                    cells[i, j].Y = j;
+
+                    // событие нажатия клавиши для каждой ячейки
+                    cells[i, j].KeyPress += cell_keyPressed;
+
+                    panel1.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+        private void cell_keyPressed(object sender, KeyPressEventArgs e)
+        {
+            var cell = sender as Sudoku;
+
+            //  ячейка заблокирована
+            if (cell.IsLocked)
+                return;
+
+            int value;
+
+            // Добавляет значение нажатой клавиши в ячейку только в том случае, если это число
+            if (int.TryParse(e.KeyChar.ToString(), out value))
+            {
+                // Clear the cell value if pressed key is zero
+                if (value == 0)
+                    cell.Clear();
+                else
+                    cell.Text = value.ToString();
+
+                cell.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+
+        private void btnGame_Click(object sender, EventArgs e)
+        {
+WrongCount = 0;
+            startNewGame();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            var wrongCells = new List<Sudoku>();
+
+            // Находит все неправильные входные данные
+            foreach (var cell in cells)
+            {
+               
+                if (!string.Equals(cell.Value.ToString(), cell.Text))
+                {
+                    wrongCells.Add(cell); 
+                } 
+            }
+
+            // Проверка, правильны ли введенные данные или игрок выигрывает
+            if (wrongCells.Any())
+               
+            {
+               
+                WrongCount += 1;
+                // Выделяет неправильные входные данные
+                wrongCells.ForEach(x => x.ForeColor = Color.Red);
+                MessageBox.Show($"Wrong inputs: {WrongCount}");
+             
+            }
+            else
+            {
+                MessageBox.Show("You Wins, Mistakes:");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            foreach (var cell in cells)
+            {
+                // Очисит ячейку, только если она не заблокирована
+                if (cell.IsLocked == false)
+                    cell.Clear();
+            }
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            startNewGame();
+            var hintsCount = 0;
+
+            // Назначает количество подсказок в зависимости от
+           // выбранного игрока уровня
+            if (beginnerLevel.Checked)
+                hintsCount = 45;
+            else if (IntermediateLevel.Checked)
+                hintsCount = 30;
+            else if (AdvancedLevel.Checked)
+                hintsCount = 15;
+
+            showRandomValuesHints(hintsCount);
+        }
+    }
+}
+
